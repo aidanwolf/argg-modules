15eeb3cad7e09c0b27371eda740302ca0eae6a4d
aidanwolf <aidan3dbit@gmail.com>
2022-05-11 23:13:52 -0700

    track colliders

--- Collidable.cs
+++ Collidable.cs
@@ -8,8 +8,12 @@ public class Collidable : Module {
 
     public string onHit {get;set;}
 
+    public List<Collider> colliders;
+
     public override void Init () {
         base.Init();
+
+        colliders = new List<Collider>();
 
         //we can specify a child object by name to be the collider
         if (!string.IsNullOrEmpty(collider)) {
@@ -22,10 +26,12 @@ public class Collidable : Module {
                 if (meshRenderer) {
                     var meshCollider = Componentizer.DoComponent<MeshCollider>(meshRenderer.gameObject,true);
                     meshCollider.convex = true;
+                    colliders.Add(meshCollider);
                 } else if (skinnedMeshRenderer) {
                     var meshCollider = Componentizer.DoComponent<MeshCollider>(skinnedMeshRenderer.gameObject,true);
                     meshCollider.sharedMesh = skinnedMeshRenderer.sharedMesh;
                     meshCollider.convex = true;
+                    colliders.Add(meshCollider);
                 }
             }
 
@@ -36,12 +42,22 @@ public class Collidable : Module {
             foreach (var meshRenderer in meshRenderers) {
                 var meshCollider = Componentizer.DoComponent<MeshCollider>(meshRenderer.gameObject,true);
                 meshCollider.convex = true;
+                colliders.Add(meshCollider);
             }
             foreach (var skinnedMeshRenderer in skinnedMeshRenderers) {
                 var meshCollider = Componentizer.DoComponent<MeshCollider>(skinnedMeshRenderer.gameObject,true);
                 meshCollider.sharedMesh = skinnedMeshRenderer.sharedMesh;
                 meshCollider.convex = true;
+                colliders.Add(meshCollider);
             }
+        }
+    }
+
+    public override void Deinit() {
+        base.Deinit();
+        var colliders = gameObject.GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders) {
+            Destroy(collider);
         }
     }
 
