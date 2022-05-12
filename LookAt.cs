2648fbb650a973705eca7796d53e9be6ce440b9b
aidanwolf <aidan3dbit@gmail.com>
2022-05-11 23:15:34 -0700

    LookAt now takes target pass, upright toggle

--- LookAt.cs
+++ LookAt.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class LookAt : Module
 {
-    public AnchorManager.Anchor target { get; set; }
+
+    private AnchorManager.Anchor targetAnchor;
+    public string target { get; set; }
     public float speed { get; set; }
+
+    public bool upright { get; set; }
 
     public override void Init () {
         base.Init();
 
-        if (target == null)
-            target = AnchorManager.GetAnchor("CROSSHAIR"); //a point 1000m out in front of the camera
+        if (string.IsNullOrEmpty(target)) {
+            init = false;
+            return;
+        }
+
+        targetAnchor = AnchorManager.GetAnchor(target); //a point 1000m out in front of the camera
 
         if (speed == 0)
             speed = 12;
@@ -21,13 +29,19 @@ public class LookAt : Module
         //if init = false, don't continue
         base.Update();
 
-        Vector3 targetDir = target.position - transform.position;
+        if (!init)
+            return;
+
+        Vector3 targetPos = targetAnchor.position;
+
+        if (upright)
+            targetPos.y = transform.position.y;
+
+        Vector3 targetDir = targetPos - transform.position;
         //targetDir.y = 0;
         float step = speed * Time.deltaTime;
  
         Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-        Debug.DrawRay(transform.position, newDir, Color.red);
- 
         transform.rotation = Quaternion.LookRotation(newDir);
     }
 
